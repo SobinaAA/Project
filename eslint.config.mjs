27a94932c46d65588024d221634e31961501e004
Alena <makkuro@gmail.com>
2025-01-25 23:22:40 +0500

    Added .husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,21 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default {
-  files: ["**/*.{js,mjs,cjs,ts}"],
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  rules: {
-    "no-unused-vars": "warn",
-    "no-undef": "warn",
-    "prefer-const": "warn",
-    "no-console": "warn"
-  },
-  ignores: [".node_modules/*"]
-};
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig, // Подключаем конфигурацию Prettier для отключения конфликтующих правил
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      'prettier/prettier': 'error'
+      // 'no-console': 'error' // Предупреждение при использовании console.log
+    }
+  }
+];
+
 
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
