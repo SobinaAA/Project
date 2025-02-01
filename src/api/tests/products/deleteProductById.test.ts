698d021a9adebdbaca431a0c778fa0f0c8d0359f
hallome85 <hallome85@mail.ru>
2025-02-02 01:06:21 +0300

    added deleteProductById, modified products.controler

--- src/api/tests/products/deleteProductById.test.ts
+++ src/api/tests/products/deleteProductById.test.ts
@@ -1,8 +1,9 @@
 import { STATUS_CODES } from '../../../data/statusCodes';
-import { test } from '../../../fixtures/apiServices.fixture';
+import { expect, test } from '../../../fixtures/apiServices.fixture';
 import { generateProductData } from '../../../data/products/generateProduct';
 import { ERRORS } from '../../../data/errorMesages';
 import { validateResponse } from '../../../utils/validation/apiValidation';
+import { TAGS } from '../../../data/tags';
 
 test.describe('[API] [Products] [DELETE the Product by Id]', async function () {
   let token = '';
@@ -13,58 +14,74 @@ test.describe('[API] [Products] [DELETE the Product by Id]', async function () {
     token = await signInApiService.getTransformedToken();
   });
 
-  test('1DE-API] Should DELETE the product by valid Id', async function ({
-    productsAPIController
-  }) {
-    const product = (
-      await productsAPIController.create(generateProductData(), token)
-    ).body.Product;
-    productId = product._id;
-    const response = await productsAPIController.delete(productId, token);
-    validateResponse(response, STATUS_CODES.DELETED, true, null);
-  });
+  test(
+    '1DE-API] Should DELETE the product by valid Id',
+    { tag: ['@1DE-API', TAGS.SMOKE, TAGS.REGRESSION] },
+    async function ({ productsAPIController }) {
+      const product = (
+        await productsAPIController.create(generateProductData(), token)
+      ).body.Product;
+      productId = product._id;
+      const response = await productsAPIController.delete(productId, token);
+      expect(response.status).toBe(STATUS_CODES.DELETED);
+      expect(response.body).toBe('');
+      const response2 = await productsAPIController.getById(productId, token);
+      validateResponse(
+        response2,
+        STATUS_CODES.NOT_FOUND,
+        false,
+        ERRORS.PRODUCT_NOT_FOUND(productId)
+      );
+    }
+  );
 
-  test('2DE-API] Should NOT DELETE the product by invalid Id', async function ({
-    productsAPIController
-  }) {
-    const response = await productsAPIController.delete(
-      token,
-      String(Date.now())
-    );
-    validateResponse(
-      response,
-      STATUS_CODES.NOT_AUTHORIZED,
-      false,
-      ERRORS.NOT_AUTHORIZED
-    );
-  });
+  test(
+    '2DE-API] Should NOT DELETE the product by invalid Id',
+    { tag: ['@2DE-API', TAGS.REGRESSION] },
+    async function ({ productsAPIController }) {
+      const response = await productsAPIController.delete(
+        token,
+        String(Date.now())
+      );
+      validateResponse(
+        response,
+        STATUS_CODES.NOT_AUTHORIZED,
+        false,
+        ERRORS.NOT_AUTHORIZED
+      );
+    }
+  );
 
-  test('3DE-API] Should NOT DELETE the product without Id', async function ({
-    productsAPIController
-  }) {
-    const response = await productsAPIController.delete(token, '');
-    validateResponse(
-      response,
-      STATUS_CODES.NOT_AUTHORIZED,
-      false,
-      ERRORS.NOT_AUTHORIZED
-    );
-  });
+  test(
+    '3DE-API] Should NOT DELETE the product without Id',
+    { tag: ['@3DE-API', TAGS.REGRESSION] },
+    async function ({ productsAPIController }) {
+      const response = await productsAPIController.delete(token, '');
+      validateResponse(
+        response,
+        STATUS_CODES.NOT_AUTHORIZED,
+        false,
+        ERRORS.NOT_AUTHORIZED
+      );
+    }
+  );
 
-  test('4DE-API] Should NOT DELETE previously deleted product', async function ({
-    productsAPIController
-  }) {
-    const product = (
-      await productsAPIController.create(generateProductData(), token)
-    ).body.Product;
-    productId = product._id;
-    const response = await productsAPIController.delete(productId, token);
-    await productsAPIController.delete(productId, token);
-    validateResponse(
-      response,
-      STATUS_CODES.NOT_FOUND,
-      false,
-      ERRORS.PRODUCT_NOT_FOUND(productId)
-    );
-  });
+  test(
+    '4DE-API] Should NOT DELETE previously deleted product',
+    { tag: ['@4DE-API', TAGS.REGRESSION] },
+    async function ({ productsAPIController }) {
+      const product = (
+        await productsAPIController.create(generateProductData(), token)
+      ).body.Product;
+      productId = product._id;
+      await productsAPIController.delete(productId, token);
+      const response = await productsAPIController.delete(productId, token);
+      validateResponse(
+        response,
+        STATUS_CODES.NOT_FOUND,
+        false,
+        ERRORS.PRODUCT_NOT_FOUND(productId)
+      );
+    }
+  );
 });
--- src/api/tests/products/getProductById.test.ts
+++ src/api/tests/products/getProductById.test.ts
@@ -8,6 +8,7 @@ import {
   validateJsonSchema
 } from '../../../utils/validation/apiValidation';
 import { validationErrorSchema } from '../../../data/jsonSchemas/validationError.shema';
+import { TAGS } from '../../../data/tags';
 
 test.describe('[API] [Products] [Get the Product by Id]', async function () {
   let token = '';
@@ -25,44 +26,50 @@ test.describe('[API] [Products] [Get the Product by Id]', async function () {
     productId = '';
   });
 
-  test('1GE-API] Should GET the product by valid Id ', async function ({
-    productsAPIController
-  }) {
-    const product = (
-      await productsAPIController.create(generateProductData(), token)
-    ).body.Product;
-    productId = product._id;
-    const response = await productsAPIController.getById(token, productId);
-    validateResponse(response, STATUS_CODES.OK, true, null);
-    validateJsonSchema(oneProductResponseSchema, response);
-  });
+  test(
+    '1GE-API] Should GET the product by valid Id ',
+    { tag: ['@1GE-API', TAGS.SMOKE, TAGS.REGRESSION] },
+    async function ({ productsAPIController }) {
+      const product = (
+        await productsAPIController.create(generateProductData(), token)
+      ).body.Product;
+      productId = product._id;
+      const response = await productsAPIController.getById(productId, token);
+      validateResponse(response, STATUS_CODES.OK, true, null);
+      validateJsonSchema(oneProductResponseSchema, response);
+    }
+  );
 
-  test('2GE-API] Should NOT GET the product by invalid Id ', async function ({
-    productsAPIController
-  }) {
-    const response = await productsAPIController.getById(
-      token,
-      String(Date.now())
-    );
-    validateResponse(
-      response,
-      STATUS_CODES.NOT_AUTHORIZED,
-      false,
-      ERRORS.NOT_AUTHORIZED
-    );
-    validateJsonSchema(validationErrorSchema, response);
-  });
+  test(
+    '2GE-API] Should NOT GET the product by invalid Id ',
+    { tag: ['@2GE-API', TAGS.REGRESSION] },
+    async function ({ productsAPIController }) {
+      const response = await productsAPIController.getById(
+        token,
+        String(Date.now())
+      );
+      validateResponse(
+        response,
+        STATUS_CODES.NOT_AUTHORIZED,
+        false,
+        ERRORS.NOT_AUTHORIZED
+      );
+      validateJsonSchema(validationErrorSchema, response);
+    }
+  );
 
-  test('3GE-API] Should NOT GET the product without Id ', async function ({
-    productsAPIController
-  }) {
-    const response = await productsAPIController.getById(token, '');
-    validateResponse(
-      response,
-      STATUS_CODES.NOT_AUTHORIZED,
-      false,
-      ERRORS.NOT_AUTHORIZED
-    );
-    validateJsonSchema(validationErrorSchema, response);
-  });
+  test(
+    '3GE-API] Should NOT GET the product without Id ',
+    { tag: ['@3GE-API', TAGS.REGRESSION] },
+    async function ({ productsAPIController }) {
+      const response = await productsAPIController.getById(token, '');
+      validateResponse(
+        response,
+        STATUS_CODES.NOT_AUTHORIZED,
+        false,
+        ERRORS.NOT_AUTHORIZED
+      );
+      validateJsonSchema(validationErrorSchema, response);
+    }
+  );
 });
