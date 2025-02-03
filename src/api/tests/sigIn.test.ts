1e9682d602668da4a6b6c1f7eff870d564ca8fbe
Tanya <ignatova2499@gmail.com>
2025-02-03 10:32:38 +0400

    Update signIn.test

--- src/api/tests/sigIn.test.ts
+++ src/api/tests/sigIn.test.ts
@@ -8,20 +8,27 @@ import {
 import { TAGS } from '../../data/tags';
 import { validationErrorSchema } from '../../data/jsonSchemas/validationError.shema';
 import { expect } from 'chai';
-import { SignInApiService } from '../services/signInApi.service';
-import { signInTestDataNegative} from "../../data/signIn/signIn.data";
+import { signInTestDataNegative } from '../../data/signIn/signIn.data';
+import { ADMIN_PASSWORD, ADMIN_USERNAME } from '../../config/env';
+import { STATUS_CODES } from '../../data/statusCodes';
 
 test.describe('[API] [Auth] [POST] [Positive]', () => {
-  const signInApiService = new SignInApiService();
-
+  const signInController = new SignInController();
   test(
-    'Should successfully login with valid credentials',
+    'Should successfully login with valid credentials and receive token in headers',
     { tag: ['@1AUTH-API', TAGS.SMOKE, TAGS.REGRESSION] },
     async () => {
-      const token = await signInApiService.loginAsAdmin()
+      const adminCredentials: IUserCredentials = {
+        username: ADMIN_USERNAME,
+        password: ADMIN_PASSWORD
+      };
+
+      const response = await signInController.login(adminCredentials);
+      validateResponse(response, STATUS_CODES.OK, true, null);
+
+      const token = response.headers['authorization'];
       expect(token).to.exist;
       expect(token).to.not.equal('');
-      expect(token).contain('Bearer ');
     }
   );
 });
@@ -33,15 +40,13 @@ test.describe('[API] [Auth] [POST] [Negative]', () => {
     signInController = new SignInController();
   });
 
-  signInTestDataNegative.forEach(({ testName, tags, data, status, ErrorMessage }) => {
-    test(
-      testName,
-      { tag: tags },
-      async () => {
+  signInTestDataNegative.forEach(
+    ({ testName, tags, data, status, ErrorMessage }) => {
+      test(testName, { tag: tags }, async () => {
         const response = await signInController.login(data as IUserCredentials);
         validateResponse(response, status, false, ErrorMessage);
         validateJsonSchema(validationErrorSchema, response);
-      }
-    );
-  });
+      });
+    }
+  );
 });
