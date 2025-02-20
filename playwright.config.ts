e7deb1d757dc82ede9df51052981aec0fda67edf
Alena <makkuro@gmail.com>
2025-02-20 17:10:30 +0500

    fixed headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
         launchOptions: {
           args: ['--start-maximized']
         },
-        headless: false
+        headless: true
         //storageState: 'src/.auth/user.json'
       },
       testDir: './src/ui/tests'
